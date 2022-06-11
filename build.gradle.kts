baaae867a1bb322a71107cab1b3a358a945b7acb
Glavo <zjx001202@gmail.com>
2022-06-11 09:41:31 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+}
+
+group = "org.glavo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.compileJava {
+    options.release.set(17)
+}
+
+tasks.jar {
+    manifest.attributes(
+        "Main-Class" to "org.glavo.rfg.Main"
+    )
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.create<JavaExec>("run") {
+    dependsOn(tasks.jar)
+    classpath = files(tasks.jar.get().archiveFile.get().asFile)
+}
---
+++ src/main/java/org/glavo/rfg/Main.java
@@ -0,0 +1,11 @@
+package org.glavo.rfg;
+
+public final class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return; // TODO: GUI
+        }
+
+
+    }
+}
